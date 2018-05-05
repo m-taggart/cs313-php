6dc7b56347724da3b6e744572c15efb535b75d5c
Taggart-McKenzie <mckenzietaggart@gmail.com>
2018-05-05 13:18:26 -0600

    added personal homepage and assignments page

---
+++ footerHome.php
@@ -0,0 +1,5 @@
+<p class="bottom"> &copy; Web Engineering II, All rights reserved. <br> All images used are believed to be in
+    "Fair Use". Please notify the author if any are not and they will be removed.</p>
+
+
+
