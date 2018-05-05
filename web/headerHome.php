4a06e5f976493f9ccfcca4cdea034814350d1615
Taggart-McKenzie <mckenzietaggart@gmail.com>
2018-05-05 13:39:03 -0600

    add web/ homepage and assingments page

---
+++ web/headerHome.php
@@ -0,0 +1,11 @@
+<div id="top">
+	<h1>CS 313 // Web Engineering II</h1>
+	<h3>McKenzie Taggart</h3>
+
+	<nav id="navigation">            
+		<ul>
+			<li><a href="home.php" title="View home page.">Home</a></li>
+			<li><a href="assignments.php" title="View Assignment Page">Assignments</a></li>
+		</ul>
+	</nav>
+</div>
